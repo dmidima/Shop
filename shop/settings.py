0bb24bc01630ee7be623c22a797c33c386d6bb79
dmidima <dmitry40646@gmail.com>
2025-08-23 02:09:38 +0300

    сделал корзину и контекст процессора

--- shop/settings.py
+++ shop/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "main",
+    "cart",
 ]
 
 MIDDLEWARE = [
@@ -42,6 +43,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                                "cart.context_processors.cart",
             ],
         },
     },
@@ -89,3 +91,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CART_SESSION_ID = 'cart'
